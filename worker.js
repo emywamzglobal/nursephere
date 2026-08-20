8b43dade94627c332905867f18fe224b7e13d891
emywamzglobal <wamalwaemily@gmail.com>
2026-08-20 19:24:16 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -27,7 +27,7 @@ export default {
     "Access-Control-Allow-Origin": "*",
 
     "Access-Control-Allow-Methods":
-        "GET, POST, DELETE, OPTIONS",
+        "GET, POST, PUT, DELETE, OPTIONS",
 
     "Access-Control-Allow-Headers":
         "Content-Type, Authorization"
